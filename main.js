5cdf3bd14166c98c70be9172b880938f0ee3f665
Yaroslav <creepers75@gmail.com>
2022-02-11 16:44:17 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,108 @@
+let canvas, ctx, flowField, flowFieldAnimation;
+
+window.onload = function () {
+    canvas = document.getElementById('canvas1')
+    ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    flowField.animate(0);
+}
+
+window.addEventListener('resize', () => {
+    cancelAnimationFrame(flowFieldAnimation)
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height)
+    flowField.animate(0)
+})
+
+const mouse = {
+    x: 0, y: 0
+}
+window.addEventListener('mousemove', function (e) {
+    mouse.x = e.x;
+    mouse.y = e.y;
+})
+
+class FlowFieldEffect {
+    #ctx;
+    #width;
+    #height;
+
+    // gradient;
+
+    constructor(ctx, width, height) {
+        this.#ctx = ctx;
+        // this.#ctx.strokeStyle = 'white';
+        this.#width = width;
+        this.#height = height;
+        this.#ctx.lineWidth = 1;
+        // this.angle = 0;
+        this.lastTime = 0;
+        this.interval = 1000 / 60;
+        this.timer = 0;
+        this.cellSiza = 30;
+        this.gradient;
+        this.#createGradient();
+        this.#ctx.strokeStyle = this.gradient;
+        this.radius = 2;
+        this.vr = 0.009;
+        // console.log('constr');
+    }
+
+    #draw(angle, x, y) {
+        let positionX = x;
+        let positionY = y;
+        let dx = mouse.x - positionX;
+        let dy = mouse.y - positionY;
+        let distance = ((dx * dx) + (dy * dy));
+        if (distance > 500000) distance = 500000;
+        else if (distance < 1000) distance = 1000;
+        let length = distance / 7000;
+        // let length = 50;
+        this.#ctx.beginPath();
+        this.#ctx.moveTo(x, y);
+        this.#ctx.lineTo(x + Math.cos(angle) * length, y + Math.sin(angle) * length);
+        this.#ctx.stroke()
+    }
+
+    #createGradient() {
+        this.gradient = this.#ctx.createLinearGradient(0, 0, this.#width, this.#height)
+        this.gradient.addColorStop(0.1, '#ff5c33')
+        this.gradient.addColorStop(0.2, '#ff66b3')
+        this.gradient.addColorStop(0.4, '#ccccff')
+        this.gradient.addColorStop(0.6, '#b3ffff')
+        this.gradient.addColorStop(0.8, '#80ff80')
+        this.gradient.addColorStop(0.9, '#ffff33')
+    }
+
+    animate(timeStamp) {
+        const deltaTime = timeStamp - this.lastTime
+        this.lastTime = timeStamp
+        if (this.timer > this.interval) {
+            // this.angle += 0.01;
+            this.#ctx.clearRect(0, 0, this.#width, this.#height)
+            this.radius += this.vr
+
+            if (this.radius > 5 || this.radius < -5) this.vr *= -1;
+
+            for (let y = 0; y < this.#height; y += this.cellSiza) {
+
+                for (let x = 0; x < this.#width; x += this.cellSiza) {
+                    const angle =
+                        (
+                        (Math.cos(x * 0.007) + Math.sin(y * 0.007))
+                    * this.radius)
+                    this.#draw(angle, x, y);
+
+                }
+            }
+            this.timer = 0;
+        } else {
+            this.timer += deltaTime
+        }
+        console.log('pablic', 100_00_23)
+        flowFieldAnimation = requestAnimationFrame(this.animate.bind(this))
+    }
+}
